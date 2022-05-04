28bdcb9781d3d3ec7f82af65d63a24bd5963c967
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-03 23:44:16 -0400

    created functions for adding, editing, viewing and deleting locations, created new page to view locations

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -3,7 +3,7 @@ import json
 import logging
 import os
 
-from flask import Blueprint, render_template, abort, url_for, current_app, jsonify
+from flask import Blueprint, render_template, abort, url_for, current_app, jsonify, flash
 from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
 
@@ -12,6 +12,8 @@ from app.db.models import Location
 from app.songs.forms import csv_upload
 from werkzeug.utils import secure_filename, redirect
 from flask import Response
+
+
 
 map = Blueprint('map', __name__,
                         template_folder='templates')
@@ -27,16 +29,20 @@ def browse_locations(page):
         return render_template('browse_locations.html',data=data,pagination=pagination)
     except TemplateNotFound:
         abort(404)
+    edit_url = ('map.edit_location', [('location_id', ':id')])
+    add_url = url_for('map.add_location')
+    delete_url = ('map.delete_location', [('location_id', ':id')])
+    return render_template('browse.html', titles=titles, add_url=add_url, edit_url=edit_url, delete_url=delete_url,
+                           retrieve_url=retrieve_url, data=data, Location=Location, record_type="Locations")
 
 @map.route('/locations_datatables/', methods=['GET'])
 def browse_locations_datatables():
-
     data = Location.query.all()
-
     try:
         return render_template('browse_locations_datatables.html',data=data)
     except TemplateNotFound:
         abort(404)
+
 
 @map.route('/api/locations/', methods=['GET'])
 def api_locations():
@@ -85,3 +91,55 @@ def location_upload():
 
 # start of new code
 
+#@map.route('/locations')
+#@login_required
+#@admin_required
+
+@map.route('/locations/<int:location_id>')
+@login_required
+def retrieve_location(location_id):
+    location = Location.query.get(location_id)
+    return render_template('location_view.html', location=location)
+
+
+@map.route('/users/<int:location_id>/edit', methods=['POST', 'GET'])
+@login_required
+def edit_locations(location_id):
+    location = Location.query.get(location_id)
+    form = location_edit_form(obj=location)
+    if form.validate_on_submit():
+        location.title = form.title.data
+        db.session.add(location)
+        db.session.commit()
+        flash('Location Edited Successfully', 'success')
+        current_app.logger.info("edited a location")
+        return redirect(url_for('map.browse_locations'))
+    return render_template('location_edit.html', form=form)
+
+
+@map.route('/locations/new', methods=['POST', 'GET'])
+@login_required
+def add_location():
+    form = location_register_form()
+    if form.validate_on_submit():
+        location = Location.query.filter_by(email=form.email.data).first()
+        if location is None:
+            location = Location(email=form.email.data, password=generate_password_hash(form.password.data))
+            db.session.add(location)
+            db.session.commit()
+            flash('Congratulations, you just created a location', 'success')
+            return redirect(url_for('map.browse_locations'))
+        else:
+            flash('Already Registered')
+            return redirect(url_for('map.locations'))
+    return render_template('location_new.html', form=form)
+
+
+@map.route('/locations/<int:location_id>/delete', methods=['POST'])
+@login_required
+def delete_location(location_id):
+    location = Location.query.get(location_id)
+    db.session.delete(location)
+    db.session.commit()
+    flash('Location Deleted', 'success')
+    return redirect(url_for('map.browse_locations'), 302)
--- app/map/forms/__init__.py
+++ app/map/forms/__init__.py
@@ -5,3 +5,33 @@ from wtforms.fields import *
 class csv_upload(FlaskForm):
     file = FileField()
     submit = SubmitField()
+
+class location_register_form(FlaskForm):
+    title = TitleField('Title', [
+        validators.DataRequired(),
+    ], description="Enter a location name")
+
+    longitude = LongitudeField('Longitude', [
+        validators.DataRequired(),
+
+    ], description="Enter a location longitude")
+    latitude = LatitudeField('Latitude', [
+        validators.DataRequired(),
+
+    ], description="Enter a location latitude")
+    population = PopulationField('Population', [
+        validators.DataRequired(),
+
+    ], description="Enter a location population")
+    submit = SubmitField()
+
+class location_edit_form(FlaskForm):
+    title = TextAreaField('Title', [validators.length(min=6, max=300)],
+                          description="Please add location title")
+    longitude = TextAreaField('Longitude', [validators.length(min=6, max=300)],
+                          description="Please add location longitude")
+    latitude = TextAreaField('Latitude', [validators.length(min=6, max=300)],
+                          description="Please add location latitude")
+    population = TextAreaField('Population', [validators.length(min=6, max=300)],
+                          description="Please add location population")
+    submit = SubmitField()
